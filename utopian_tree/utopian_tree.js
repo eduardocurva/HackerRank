6c98d86446ba56ddae802f77a9db070e637db57b
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-03-13 17:56:31 +1300

    code review - fixing variable/function names

--- utopian_tree/utopian_tree.js
+++ utopian_tree/utopian_tree.js
@@ -1,21 +1,21 @@
 function main() {
-    var t = parseInt(readLine());
+    var total_test_case = parseInt(readLine());
     var finalHeight = 1;
-    for (var a0 = 0; a0 < t; a0++) {
-        var n = parseInt(readLine());
+    for (var i = 0; i < total_test_case; i++) {
+        var total_cycle = parseInt(readLine());
 
-        console.log(calcHeight(n));
+        console.log(getHeight(total_cycle));
 
     }
 }
 
-function calcHeight(n) {
+function getHeight(total_cycle) {
     var initialHeight = 1;
     var finalHeight = 0;
-    if (n === 0)
+    if (total_cycle === 0)
         return initialHeight;
 
-    for (var i = 0; i < n; i++) {
+    for (var i = 0; i < total_cycle; i++) {
         if (i % 2 === 0) {
             finalHeight = doSpring(initialHeight);
         } else {
@@ -27,12 +27,13 @@ function calcHeight(n) {
     return finalHeight;
 
 }
+
 // spring, it doubles in height. 
-function doSpring(h) {
-    return h * 2;
+function doSpring(height) {
+    return height * 2;
 }
 
 //Each summer, its height increases by 1 meter.
-function doSummer(h) {
+function doSummer(height) {
     return h + 1;
 }
