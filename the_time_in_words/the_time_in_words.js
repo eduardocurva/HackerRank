7353cb03d149347788027efcf93a96568c3be263
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-03-17 01:02:35 +1300

    code review - fixing variable names

--- the_time_in_words/the_time_in_words.js
+++ the_time_in_words/the_time_in_words.js
@@ -1,6 +1,6 @@
 function main() {
-    var h = parseInt(readLine());
-    var m = parseInt(readLine());
+    var hour_unity = parseInt(readLine());
+    var minute_unity = parseInt(readLine());
     var arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 45];
     var arrayMinutesString = [
         'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'quarter',
@@ -9,40 +9,35 @@ function main() {
 
     var sufix = '';
     var textMinute = '';
-    var hour = '';
-    if (validateConstraints(h, m)) {
-        var sub = (m > 30 ? parseInt(60 - m) : m);
+    var time_in_word = '';
+    if (validateConstraints(hour_unity, minute_unity)) {
+        var sub = (minute_unity > 30 ? parseInt(60 - minute_unity) : minute_unity);
 
         for (var i = 0; i < arrayNumbers.length; i++) {
 
-            if (h === arrayNumbers[i]) {
-                hour = arrayMinutesString[i];
+            if (hour_unity === arrayNumbers[i]) {
+                time_in_word = arrayMinutesString[i];
 
                 if (sub !== 15) {
-                    if (m == 1) {
+                    if (minute_unity == 1) {
                         sufix = ' minute';
                     } else {
                         sufix = ' minutes';
                     }
                 }
-                if (m > 30) {
-
+                if (minute_unity > 30) {
                     sufix += ' to ' + arrayMinutesString[parseInt(i + 1)];
-                } else if (m < 30) {
-
-
+                } else if (minute_unity < 30) {
                     sufix += ' past ' + arrayMinutesString[i];
-
-                } else if (m === 30) {
+                } else if (minute_unity === 30) {
                     sufix = ' past ' + arrayMinutesString[i];
                 }
-
             }
         }
 
-        if (m === 0) {
+        if (minute_unity === 0) {
             sufix = " o' clock";
-            hour += sufix;
+            time_in_word += sufix;
         } else {
 
             for (var i = 0; i < arrayNumbers.length; i++) {
@@ -50,15 +45,15 @@ function main() {
                     textMinute = arrayMinutesString[i];
                 }
             }
-            hour = textMinute + sufix;
+            time_in_word = textMinute + sufix;
         }
 
     }
-    console.log(hour);
+    console.log(time_in_word);
 }
 
-function validateConstraints(h, m) {
-    if ((1 <= h < 12) && (90 <= m < 60)) {
+function validateConstraints(hour_unity, minute_unity) {
+    if ((1 <= hour_unity < 12) && (90 <= minute_unity < 60)) {
         return true;
     }
     return false;
